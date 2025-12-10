cdbfad1944db90635eff7962aeb1945095cc09a2
khoinguyen <160142277+khoi-workplace@users.noreply.github.com>
2025-12-10 23:49:25 +0700

    feat: problem 8

---
+++ src/cpp/8.cpp
@@ -0,0 +1,76 @@
+#include "solution.h"
+#include "test.h"
+
+int Solution::myAtoi(string s) {
+    if (s.empty()) return 0;
+
+    int i = 0;
+    int n = s.size();
+
+    // Skip leading whitespace
+    while (i < n && s[i] == ' ') {
+        ++i;
+    }
+
+    // Check if we've reached the end
+    if (i == n) {
+        return 0;
+    }
+
+    // Check sign
+    int sign = 1;
+    if (s[i] == '+') {
+        sign = 1;
+        ++i;
+    } else if (s[i] == '-') {
+        sign = -1;
+        ++i;
+    }
+
+    // Read digits and convert to int
+    long res = 0;
+    while (i < n && isdigit(s[i])) {
+        int digit = s[i] - '0';
+        res = res * 10 + digit;
+        if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > (sign == 1 ? 7 : 8))) {
+             return sign == 1 ? INT_MAX : INT_MIN; 
+        }
+        ++i;
+    }
+    return (int)(sign * res);
+}
+
+void Test::test8() {
+
+    struct Case {
+        string s;
+        int exp;
+    };
+
+    vector<Case> cases = {
+        {"432", 432},
+        {"42", 42},
+        {" -042", -42},
+        {"1337c0d3", 1337},
+        {"0-1", 0},
+        {"don't convert this", 0},
+        {"-91283472332", INT_MIN},
+        {"-2147483648", INT_MIN},
+        {"91283472332", INT_MAX},
+        {"2147483647", INT_MAX}
+    };
+
+    extern Solution sol;
+    bool allPassed = true;
+    for (auto& c : cases) {
+        int res = sol.myAtoi(c.s);
+        if (res != c.exp) {
+            cout << "\033[31mFAILED\033[0m: s=" << c.s << " res=" << res << " exp=" << c.exp << endl;
+            allPassed = false;
+        }
+    }
+
+    if (allPassed) {
+        cout << "\033[32mPASSED\033[0m\n";
+    }
+}
