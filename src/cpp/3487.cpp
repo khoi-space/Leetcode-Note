0e79a65e756713db36ae38cd6ae59033c493c480
khoinguyen <160142277+khoi-workplace@users.noreply.github.com>
2025-12-13 11:12:43 +0700

    feat: #3487

---
+++ src/cpp/3487.cpp
@@ -0,0 +1,49 @@
+#include "solution.h"
+#include "test.h"
+using namespace std;
+
+/**
+ * Problem 3487: Maximum Unique Subarray Sum After Deletion
+ * @input: integer array
+ * @output: maximum sum of a subarray
+ * @rule:
+ * - All elements in the subarray are unique.
+ * - The sum of the elements in the subarray is max.
+ */
+int Solution::maxSum(vector<int>& nums) {
+    unordered_set<int> positiveNumSet;
+    int max_sum = 0;
+    for (int num : nums) {
+        if (num > 0) {
+            positiveNumSet.insert(num);
+        }
+    }
+
+    if (positiveNumSet.empty()) {
+        return *max_element(nums.begin(), nums.end());
+    }
+    
+    for (int positive_num : positiveNumSet) {
+        max_sum += positive_num;
+    }
+    return max_sum;
+}
+
+void Test::test3487() {
+    struct Case {
+        vector<int> nums;
+        int exp;
+    };
+
+    vector<Case> cases = {
+        {{1,2,3,4,5}, 15}, //0
+        {{1,1,0,1,1}, 1}, //1
+        {{1,2,-1,-2,1,0,-1}, 3} //2
+    };
+
+    extern Solution sol;
+    for (int i = 0; i < (int)cases.size(); ++i) {
+        int res = sol.maxSum(cases[i].nums);
+        Test::assertTest(res, cases[i].exp, i);
+    }
+}  
