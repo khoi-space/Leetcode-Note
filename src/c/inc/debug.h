80b922bf99c647edaefb18b0a73b432fc00ee975
khoinguyen <160142277+khoi-workplace@users.noreply.github.com>
2026-01-06 13:51:14 +0700

    refractor: debug

--- src/c/inc/debug.h
+++ src/c/inc/debug.h
@@ -2,16 +2,31 @@
 #define DEBUG_H
 
 #include <stdio.h>
-
-// Uncomment to turn off debug mode
-#define DEBUG
+#include <stdbool.h>
 
 #ifdef DEBUG
-    #define db_see(x) printf("%s: %d\n", #x, (x))
-    #define db_say(msg) printf("%s\n", msg)
+    /**
+     * Usage:
+     *  db(msg)         : to shout a useless msg
+     *  db_info(msg)    : to shout a info msg
+     *  db_warn(msg)    : to shout a warnign msg (unusual behavior)
+     *  db_error(msg)   : to shout a error msg (to track before crash)
+     *  db_var(x)       : to dump name and value of x
+     *  db_loc()        : to track the line and file
+     */
+    #define db(msg)         printf("[DEBUG] %s\n", msg);
+    #define db_info(msg)    printf("\033[7m[INFO]\033[0m %s\n", msg);
+    #define db_warn(msg)    printf("\033[33m[WARN]\033[0m %s\n", msg);
+    #define db_error(msg)   printf("\033[31m[ERROR]\033[0m %s\n", msg);
+    #define db_var(x)       printf("[VAR] %s = %d\n", #x, (x));
+    #define db_loc()        printf("[LOC] %s:%d\n", __FILE__, __LINE__)
 #else
-    #define db_see(x)
-    #define db_say(msg)
+#define db(msg)
+    #define db_info(msg)
+    #define db_warn(msg)
+    #define db_error(msg)
+    #define db_var(x)  
+    #define db_loc()
 #endif
 
 #endif // DEBUG_H
