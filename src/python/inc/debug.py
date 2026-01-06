289bd31700785d6f46ec6e63ba8327ddf6b57aaa
khoinguyen <160142277+khoi-workplace@users.noreply.github.com>
2026-01-06 23:03:23 +0700

    feat: python debug

--- src/python/inc/debug.py
+++ src/python/inc/debug.py
@@ -1,20 +1,33 @@
+import inspect
 DEBUG = False
 
+def loc(endl=True):
+    """
+    Print location of where loc() called in format "__file__:__line__"
+    """
+    if globals.get('DEBUG', False):
+        frame = inspect.currentframe().f_back
+        if endl:
+            print(f"{frame.f_code.co_filename}:{frame.f_lineno}")
+        else:
+            print(f"{frame.f_code.co_filename}:{frame.f_lineno}", end=" | ")
+
 def dump(*args, endl=True, **kwargs):
-    """Print value with a short expression"""
+    """
+    Print arguments
+
+    :example:
+        dump(a)                       : a
+        dump(a, b, c)                 : a b c 
+        dump("x=", x)                 : x=<x>
+        dump([1,2,3])                 : [1, 2, 3]
+        dump(a, b, sep=", ")          : 1, 2, 3
+    """
     if globals().get('DEBUG', False):
         if endl:
             print(*args, **kwargs)
         else:
             print(*args, **kwargs, end=" | ")
-
-def see(*args, endl=True):
-    """Just print the value"""
-    if globals().get('DEBUG', False):
-        if endl:
-            print(*args)
-        else:
-            print(*args, end=" | ")
 
 def log(*args, endl=True, **kwargs):
     """Print the value with short expression into debug.log"""
