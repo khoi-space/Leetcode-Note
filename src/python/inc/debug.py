fc1b09bd493c0452d81acef494e9ae820f3fe2ba
khoinguyen <160142277+khoi-workplace@users.noreply.github.com>
2026-01-06 13:49:13 +0700

    feat: debug

--- src/python/inc/debug.py
+++ src/python/inc/debug.py
@@ -1,15 +1,26 @@
 DEBUG = False
 
-def see(*args, endl=True, **kwargs):
+def dump(*args, endl=True, **kwargs):
+    """Print value with a short expression"""
     if globals().get('DEBUG', False):
         if endl:
             print(*args, **kwargs)
         else:
             print(*args, **kwargs, end=" | ")
 
-def say(*args, endl=True):
+def see(*args, endl=True):
+    """Just print the value"""
     if globals().get('DEBUG', False):
         if endl:
             print(*args)
         else:
             print(*args, end=" | ")
+
+def log(*args, endl=True, **kwargs):
+    """Print the value with short expression into debug.log"""
+    if globals().get('DEBUG', False):
+        with open('debug.log', 'a', encoding='utf-8') as f:
+            if endl:
+                print(*args, **kwargs, file=f)
+            else:
+                print(*args, **kwargs, end=" | ", file=f)
