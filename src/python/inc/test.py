bd149ffc7ab9801626c0ad8eb3c07531b2743e76
khoinguyen <160142277+khoi-workplace@users.noreply.github.com>
2026-01-06 23:22:43 +0700

    feat: unordered compare

--- src/python/inc/test.py
+++ src/python/inc/test.py
@@ -1,26 +1,37 @@
-def assertTest(res, exp, case_name):
+from typing import List, Optional, Dict, Set, Tuple
+from inc.listnode import *
+from inc.treenode import *
+
+def isEqual(lhs, rhs, unorder=False) -> bool:
+	"""
+	Compare lhs and rhs
+	
+	:param lhs, rhs: arguments
+	:param unorder: don't care about order
+	"""
+	if is_listnode(lhs) and is_listnode(rhs):
+		# Handle with listnode
+		return listnode_equal(lhs, rhs, unorder)
+	elif is_treenode(lhs) and is_treenode(rhs):
+		# Handle with treenode
+		return treenode_equal(lhs, rhs, unorder)
+	else:
+		# Handle with basic types
+		if unorder and isinstance(lhs, (list, tuple, str) and isinstance(rhs, (list, tuple, str))):
+			# Sort first
+			sorted(lhs)
+			sorted(rhs)
+
+		return lhs == rhs
+
+def assertTest(res, exp, case_name, unorder=False):
 	GREEN = '\033[1;32m'
 	RED = '\033[1;31m'
 	RESET = '\033[0m'
 	BOLD = '\033[1m'
 	print(f"{BOLD}Case {case_name + 1}: ", end='')
 
-	def is_listnode(obj):
-		return hasattr(obj, 'val') and hasattr(obj, 'next')
-
-	def listnode_equal(a, b):
-		while a and b:
-			if a.val != b.val:
-				return False
-			a = a.next
-			b = b.next
-		return a is None and b is None
-
-	equal = False
-	if is_listnode(res) and is_listnode(exp):
-		equal = listnode_equal(res, exp)
-	else:
-		equal = (res == exp)
+	equal = isEqual(res, exp, unorder)
 
 	if not equal:
 		print(f"{RED}FAILED{RESET}")
